3284e0afa500080a2826365d53eed936872f2f21
Brendan Lambrecht <blambrecht04@gmail.com>
2025-02-04 16:35:49 -0600

    Add files via upload

---
+++ src/app/pictures/pictures.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pictures',
+  imports: [],
+  templateUrl: './pictures.component.html',
+  styleUrl: './pictures.component.scss'
+})
+export class PicturesComponent {
+
+}
---
+++ src/app/research-projects/research-projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-research-projects',
+  imports: [],
+  templateUrl: './research-projects.component.html',
+  styleUrl: './research-projects.component.scss'
+})
+export class ResearchProjectsComponent {
+
+}
