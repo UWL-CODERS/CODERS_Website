a82b0d1325eabf36d856dbbcf4a8807e949c0cbd
bishalkarki01 <63300213+bishalkarki01@users.noreply.github.com>
2025-02-11 21:01:39 -0600

    Navigation Bar and Router links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,29 @@
 // Do Not Modify Yet. To Include Page Routing for Website!
 
 import { Routes } from '@angular/router';
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { AboutUsComponent } from './about-us/about-us.component';
-// import { MainComponent } from './main/main.component';
-// import { OpportunitiesComponent } from './opportunities/opportunities.component';
-// import { PicturesComponent } from './pictures/pictures.component';
-// import { ResearchProjectsComponent } from './research-projects/research-projects.component';
+//import { NgModule } from '@angular/core';
+//import { RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MainComponent } from './main/main.component';
+import { OpportunitiesComponent } from './opportunities/opportunities.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { ResearchProjectsComponent } from './research-projects/research-projects.component';
+import { LayoutComponent } from './shared/layout/layout.component';
 
 
-// export const routes: Routes = [
-//   { path: '', component: HomeComponent },
-//   { path: 'home', component: HomeComponent },
-//   { path: 'contact', component: ContactComponent },
-//   { path: 'job-info', component: JobInfoComponent },
-//   { path: 'my-work', component: MyWorkComponent },
-//   { path: '**', redirectTo: '' }  // Redirect any unknown routes to home
-// ];
+export const routes: Routes = [
+    {
+    path: '',component: LayoutComponent,children: [
+    { path: '', component: MainComponent },
+    { path: 'home', component: MainComponent },
+    { path: 'opportunities', component: OpportunitiesComponent },
+    { path: 'research', component: ResearchProjectsComponent },
+    { path: 'pictures', component: PicturesComponent },
+    { path: 'aboutus', component: AboutUsComponent }
+        ]}
+    ];
 
-export const routes: Routes = [];
+
 
 // @NgModule({
 //   imports: [RouterModule.forRoot(routes, {
