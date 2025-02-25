63b10b031c8c0cace006215da75dd9a2789a17eb
Muhammad Fardeen <m.fdeen02@gmail.com>
2025-02-25 13:36:32 -0600

    refactor: update routing to use loadComponent for lazy loading of modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,23 @@
 import { Routes } from '@angular/router';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { MainComponent } from './main/main.component';
-import { OpportunitiesComponent } from './opportunities/opportunities.component';
-import { PicturesComponent } from './pictures/pictures.component';
-import { ResearchProjectsComponent } from './research-projects/research-projects.component';
-import { LayoutComponent } from './shared/layout/layout.component';
+
+
+
+
+
+
 
 
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
+    loadComponent: () => import('./shared/layout/layout.component').then(m => m.LayoutComponent),
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: MainComponent },
-      { path: 'opportunities', component: OpportunitiesComponent },
-      { path: 'research', component: ResearchProjectsComponent },
-      { path: 'pictures', component: PicturesComponent },
-      { path: 'aboutus', component: AboutUsComponent }
+      { path: 'home', loadComponent: () => import('./main/main.component').then(m => m.MainComponent) },
+      { path: 'opportunities', loadComponent: () => import('./opportunities/opportunities.component').then(m => m.OpportunitiesComponent) },
+      { path: 'research', loadComponent: () => import('./research-projects/research-projects.component').then(m => m.ResearchProjectsComponent) },
+      { path: 'pictures', loadComponent: () => import('./pictures/pictures.component').then(m => m.PicturesComponent) },
+      { path: 'aboutus', loadComponent: () => import('./about-us/about-us.component').then(m => m.AboutUsComponent) }
     ],
   }
 ];
