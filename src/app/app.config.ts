dd8c4da465f0d0a89418029d833a28ac254fc295
Muhammad Fardeen <m.fdeen02@gmail.com>
2025-03-11 20:55:36 -0500

    feat: Configure PrimeNG with Nora theme and enable animations
    Added PrimeNG configuration to app.config.ts, specifying the Nora theme. Enabled animations using provideAnimationsAsync. This integrates PrimeNG's UI components and improves the application's visual experience.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,23 @@
 // DO NOT MODIFY THIS!!!
 
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration, withIncrementalHydration } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Nora from '@primeng/themes/nora';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withIncrementalHydration())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withIncrementalHydration()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Nora,
+      },
+    }),
+  ],
 };
