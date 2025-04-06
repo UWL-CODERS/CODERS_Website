8a181612c4483d9a542a030b279a9fcc666f1485
Muhammad Fardeen <m.fdeen02@gmail.com>
2025-04-05 20:34:39 -0500

    refactor: relocate preset file and update path in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter, withPreloading, PreloadAllModules } from '@angular/route
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
-import { CODERS_Preset } from './components/coders.preset';
+import { CODERS_Preset } from '../presets/coders.preset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
