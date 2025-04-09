f6bd56415fdd3a77851909ab18b7452ffe7ec3ac
Muhammad Fardeen <m.fdeen02@gmail.com>
2025-04-08 19:21:24 -0500

    refactor(gallery): update template syntax and remove unused imports

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,9 +1,9 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pictures',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
