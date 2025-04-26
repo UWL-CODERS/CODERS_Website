1f025a4167e10d6e8bcf45e0283fc23812de9304
Muhammad Fardeen <m.fdeen02@gmail.com>
2025-04-25 21:24:40 -0500

    refactor(carousel): update visible items and scrolling behavior

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -16,16 +16,11 @@ export class CarouselComponent {
   responsiveOptions: any[] = [
       {
           breakpoint: '1199px',
-          numVisible: 3,
-          numScroll: 3
+          numVisible: 2,
+          numScroll: 1
       },
       {
           breakpoint: '991px',
-          numVisible: 2,
-          numScroll: 2
-      },
-      {
-          breakpoint: '767px',
           numVisible: 1,
           numScroll: 1
       }
