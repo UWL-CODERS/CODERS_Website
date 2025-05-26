4c845884a919282ec53e5d2b11515fde9cd85bde
Muhammad Fardeen <muhd@fdeen.com>
2025-05-26 14:46:52 -0500

    refactor(app.config): remove redundant PageTitleStrategy and related imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,15 @@
-import { ApplicationConfig, inject, Injectable, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideClientHydration, Title, withIncrementalHydration } from '@angular/platform-browser';
-import { provideRouter, withPreloading, PreloadAllModules, RouterStateSnapshot, TitleStrategy } from '@angular/router';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideClientHydration, withIncrementalHydration } from '@angular/platform-browser';
+import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import { CODERS_Preset } from '../presets/coders.preset';
 
-@Injectable({ providedIn: 'root' })
-export class PageTitleStrategy extends TitleStrategy {
-  private readonly title = inject(Title);
-  override updateTitle(routerState: RouterStateSnapshot) {
-    const title = this.buildTitle(routerState);
-    if (title !== undefined) {
-      this.title.setTitle(`CODERS Club | ${title}`);
-    }
-  }
-}
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    { provide: TitleStrategy, useClass: PageTitleStrategy },
     provideClientHydration(withIncrementalHydration()),
     provideAnimationsAsync(),
     providePrimeNG({
