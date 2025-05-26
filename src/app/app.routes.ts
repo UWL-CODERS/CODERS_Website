a56e0154d984d52ffe8badf55a619d05edf170a3
Muhammad Fardeen <muhd@fdeen.com>
2025-05-26 14:37:21 -0500

    refactor(routes): revert component to loadComponent for lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,22 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './components/layout/layout.component';
-import { HomeComponent } from './pages/home/home.component';
-import { OpportunitiesComponent } from './pages/opportunities/opportunities.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { GalleryComponent } from './pages/gallery/gallery.component';
-import { AboutComponent } from './pages/about/about.component';
+
+
+
+
+
+
 
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
+    loadComponent: () => import('./components/layout/layout.component').then(m => m.LayoutComponent),
     children: [
-        { path: '', component: HomeComponent },
-        { path: 'opportunities', component: OpportunitiesComponent },
-        { path: 'projects', component: ProjectsComponent },
-        { path: 'gallery', component: GalleryComponent },
-        { path: 'about', component: AboutComponent },
-        { path: '**', component: HomeComponent }
+        { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+        { path: 'opportunities', loadComponent: () => import('./pages/opportunities/opportunities.component').then(m => m.OpportunitiesComponent) },
+        { path: 'projects', loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent) },
+        { path: 'gallery', loadComponent: () => import('./pages/gallery/gallery.component').then(m => m.GalleryComponent) },
+        { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
+        { path: '**', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) }
     ],
   }
 ];
